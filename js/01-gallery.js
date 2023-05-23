509a0f91abc7a46bab4791a19bdf93b16dd24a14
andrenaP <a0993100106@gmail.com>
2023-05-23 11:27:09 +0300

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,32 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const ULgallery = document.querySelector(".gallery");
+ULgallery.innerHTML = CreateImageCardsMarkup(galleryItems);
+function CreateImageCardsMarkup(Items) {
+    return Items
+        .map(({ preview, original, description }) => {
+            return `
+<li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+        })
+        .join('');
+}
+ULgallery.addEventListener('click', onClick)
+function onClick(event) {
+    event.preventDefault()
+    if (event.target.nodeName != 'IMG')
+        return;
+    const instance = basicLightbox.create(`<img src="${event.target.dataset.source}">`);
+    instance.show();
+}
