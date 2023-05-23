ab194788ccb2c2530a2c5e968a3f69bfb59ebed0
andrenaP <a0993100106@gmail.com>
2023-05-23 19:35:23 +0300

    final

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryCardsSet = createGallery(galleryItems);
+
+function createGallery(galleryItems) {
+    return galleryItems
+        .map(({ original, preview, description }) => {
+            return `<li class="gallery__item" >
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>
+</li>`;
+        })
+        .join('');
+}
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryCardsSet);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    caption: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+});
